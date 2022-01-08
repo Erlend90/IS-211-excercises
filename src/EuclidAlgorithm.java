13b51242301a514ddfa78b6f81905b0c40cdd1de
Erlend90 <erlend.lotsberg@gmail.com>
2022-01-08 18:40:40 +0100

    Fungerende Euklid-algoritme, forhåndsdefinerte args

---
+++ src/EuclidAlgorithm.java
@@ -0,0 +1,22 @@
+public class EuclidAlgorithm {
+    public static void main(String[] args) {
+        int firstInt = Integer.parseInt(args[0]);
+        int secondInt = Integer.parseInt(args[1]);
+        int rem = 1;
+        int k = 0;
+
+        if (firstInt < secondInt) {
+            int tempInt = firstInt;
+            firstInt = secondInt;
+            secondInt = tempInt;
+        }
+        while (rem > 0) {
+            rem = firstInt % secondInt;
+            k++;
+            firstInt = secondInt;
+            secondInt = rem;
+        }
+        System.out.println("Iterations before remainder is 0 = " + k);
+    }
+}
+
